f3a9082e27535c946896558be0079af9de2d11f1
Joseph Niel Tuazon <josephnieltuazon@yahoo.com>
2019-03-09 21:45:54 +0800

    Remove unused controller

--- server/app/bootstrap.py
+++ server/app/bootstrap.py
@@ -3,7 +3,6 @@ from flask_pymongo import PyMongo
 from flask_socketio import SocketIO
 
 from app.config import MONGODB_URI
-from app.router import ROUTES
 from app.utils import import_submodules
 
 
@@ -15,15 +14,6 @@ def create_app() -> Flask:
     app = Flask(__name__)
 
     mongo.init_app(app, uri=MONGODB_URI)
-
-    import_submodules('app.controllers')
-    app.url_map.strict_slashes = False
-    for route in ROUTES:
-        app.add_url_rule(
-            route.location,
-            view_func=route.view_func,
-        )
-
     socketio.init_app(app)
     import_submodules('app.events')
 
